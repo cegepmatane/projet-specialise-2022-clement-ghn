43910df5c40fa559c68debe4b9483736d597e79d
clement-ghn <93922830+clement-ghn@users.noreply.github.com>
2022-02-14 17:00:29 -0500

    Application de la Poc à mon projet
    Après avoir essayer ma Poc sur un exemple similaire au tutorielsuivi, j'applique cette utilisation sur mon projet, afin de réussir ma Poc

--- src/main/java/com/example/ktp/Activity2.java
+++ src/main/java/com/example/ktp/Activity2.java
@@ -1,14 +1,29 @@
 package com.example.ktp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity2 extends AppCompatActivity {
 
+    private String partie = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+
+        //ouverture de la BDD
+        final DatabaseAccess db = DatabaseAccess.getInstance(getApplicationContext());
+        db.open();
+
+        partie = db.getpartie("jambes");
+
+        TextView databasetest = (TextView) findViewById(R.id.databasetest);
+        databasetest.setText(partie);
+
+
+        db.close();
     }
 }
